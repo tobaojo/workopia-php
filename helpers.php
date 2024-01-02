76ed6ecd4c66531bbb7116177642cac512d10cdd
tobaojo <toba_ojo@hotmail.com>
2024-01-02 11:52:32 +0000

    refactored to use paths

--- helpers.php
+++ helpers.php
@@ -10,3 +10,41 @@ function basePath($path = '')
 {
     return __DIR__ . '/' . $path;
 }
+
+
+/**
+ * load a view
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name)
+{
+
+    $viewPath = basePath("views/{$name}.view.php");
+
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View {$name} not found!";
+    }
+}
+
+
+
+/**
+ * load a partial
+ * @param string $name
+ * @return void
+ */
+
+function loadPartial($name)
+{
+    $partialPath =  basePath("views/partials/{$name}.php");
+
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial {$name} not found!";
+    }
+}
